ab7a19b9294935ee6f6aed26dd6c5445fcf914b6
Logan Harris <loganrodharris@gmail.com>
2020-09-24 14:10:32 +1000

    added tests for getword module

--- src/test/test_getword.py
+++ src/test/test_getword.py
@@ -1,12 +1,16 @@
 import unittest
 import sys
 import os
+import tracemalloc
 sys.path.append(f'{os.path.dirname(os.path.abspath(__file__))}\..')
 from get_word import GetWord
 
 class TestGetWord(unittest.TestCase):
     def test_from_api(self):
-        pass
+        test = GetWord.from_api()
+        self.assertEqual(type(test), str)
 
     def test_from_wordlist(self):
-        pass
+        tracemalloc.start()
+        test = GetWord.from_wordlist()
+        self.assertEqual(type(test), str)
